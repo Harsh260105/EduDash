eee6585a9b22d9a802521c1be4df5ec43698281c
Het patel <hetbpatel203@gmail.com>
2025-07-17 10:35:28 +0530

    Refactor Announcements and EventCalendar components to integrate data fetching and improve UI presentation

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const EventCalendar = () => {
-  return (
-    <div>EventCalendar</div>
-  )
-}
+  const [value, onChange] = useState<Value>(new Date());
 
-export default EventCalendar
+  const router = useRouter();
+
+  useEffect(() => {
+    if (value instanceof Date) {
+      router.push(`?date=${value}`);
+    }
+  }, [value, router]);
+
+  return <Calendar onChange={onChange} value={value} />;
+};
+
+export default EventCalendar;
